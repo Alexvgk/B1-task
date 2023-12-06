a33515e66a0d771d0146e1775acf169226229659
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-12-06 12:48:11 +0300

    model added

---
+++ Console-task/Conbination/Model/DataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Test.Conbination.Model
+{
+    public class DataModel : BaseModel
+    {
+        public DateTime Date { get; set; }
+        public string LatinSymbols { get; set; }
+        public string RussianSymbols { get; set; }
+        public long IntegerNumber { get; set; }
+        public double FloatingPointNumber { get; set; }
+    }
+}
