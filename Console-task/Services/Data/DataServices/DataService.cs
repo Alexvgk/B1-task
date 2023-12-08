8bc2ff764dd49f388144b065a234fb2a151d8d7f
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-12-09 00:29:02 +0300

    structure of working with DB has been changed

--- Console-task/Services/Data/DataServices/DataService.cs
+++ Console-task/Services/Data/DataServices/DataService.cs
@@ -8,34 +8,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
+using Console_Test.Services.Data.Procedures;
+using Console_Test.Services.Parsing;
 
 namespace Console_Test.Services.Data.DataServices
 {
-    public class DataService : BaseDataService<DataModel>
+    public class DataService : BaseDataService<DataModel>, IRepository<DataModel>, IProceduresAccessObject<DataModel>
     {
-        IRepository<DataModel> repository;
 
         public DataService() : base()
         {
-            this.repository = new DataRepo();
         }
 
-
-        public void setData(List<DataModel> data)
+        // Deletes data based on the specified identifier
+        public void deleteData(int id)
         {
-           this.repository.insertData(data);
+            throw new NotImplementedException();
         }
 
+        // Retrieves a list of all data
+        public List<DataModel> getData()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Retrieves data based on the specified identifier
+        public DataModel getDataById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Inserts data into the database using SqlBulkCopy
+        public void insertData(List<DataModel> data)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection))
+                    {
+                        // Set the destination table for the bulk copy operation
+                        bulkCopy.DestinationTableName = "DataTable";
+
+                        // Configure bulk copy settings
+                        bulkCopy.NotifyAfter = 500;
+                        bulkCopy.SqlRowsCopied += (sender, e) => OnSqlRowsCopied(e, data.Count);
+
+                        // Convert the list of data models to a DataTable
+                        DataTable dataTable = ParsingService.ParseToDataTable(data);
+
+                        // Execute the bulk copy operation
+                        bulkCopy.WriteToServer(dataTable);
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                // Handle SQL-specific exceptions
+                throw;
+            }
+            catch (Exception)
+            {
+                // Handle general exceptions
+                throw;
+            }
+        }
+
+        // Updates data based on the specified identifier
+        public void updateData(int id, DataModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Calculates the sum and median of the data
         public void calculateSumAndMedian(out long sum, out double median)
         {
-
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
 
                 using (SqlCommand command = new SqlCommand(SqlQueries.createProcedureScript, connection))
                 {
+                    // Create and execute a stored procedure to calculate sum and median
                     command.ExecuteNonQuery();
+
+                    // Retrieve the results of the stored procedure
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         if (reader.Read())
@@ -51,6 +110,17 @@ namespace Console_Test.Services.Data.DataServices
                     }
                 }
             }
+        }
+
+        // Event handler for the SqlRowsCopied event, displaying information about the copied rows
+        private void OnSqlRowsCopied(SqlRowsCopiedEventArgs e, int totalRowCount)
+        {
+            Console.CursorVisible = false;
+
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write($"Copied {e.RowsCopied} rows. Remaining to copy: {totalRowCount - e.RowsCopied}.");
+
+            Console.CursorVisible = true;
         }
     }
 }
