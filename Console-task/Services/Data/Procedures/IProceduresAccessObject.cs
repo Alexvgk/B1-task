881c706568bc2b749701f1946ffffd0bbadbf337
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-12-09 00:30:31 +0300

    procedure interface created

---
+++ Console-task/Services/Data/Procedures/IProceduresAccessObject.cs
@@ -0,0 +1,14 @@
+﻿using Console_Test.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Test.Services.Data.Procedures
+{
+    public interface IProceduresAccessObject<T> where T : BaseModel
+    {
+        public void calculateSumAndMedian(out long sum, out double median);
+    }
+}
